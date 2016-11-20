42f93e9aa5293610e87898bc4186b2a550d514d4
naR90 <Nadyuha-Revgan@yandex.ru>
2016-11-20 17:11:37 +0200

    something

---
+++ Math/Math/Vector.cpp
@@ -0,0 +1,12 @@
+#include "Vector.h"
+
+Vector2::Vector2()
+{
+  X = Y = 0.0f;
+}
+
+Vector2::Vector2(float _X, float _Y)
+{
+  X = _X;
+  Y = _Y;
+}
---
+++ Math/Math/Vector.h
@@ -0,0 +1,16 @@
+#pragma once
+
+class Vector2
+{
+public:
+  Vector2();
+
+  Vector2(
+	  float _X,
+	  float _Y
+    );
+
+protected:
+  float X;
+  float Y;
+};
